7aa8e03d037fa54401d6a5870012563aa8983dcd
Anshuman <okanshuman@gmail.com>
2025-01-03 20:23:07 +0530

    Uncommented a ccode

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ api = ShoonyaApiPy()
 api.login(userid=cr.user, password=cr.pwd, twoFA=cr.factor2, vendor_code=cr.vc, api_secret=cr.app_key, imei=cr.imei)
 
 # Fetch Current Price
-#symbolName='Reliance'
-#currentPrice = getPriceBySymbolName(api, tradingSymbolName=symbolName)
-#print(f"Current Price of {symbolName} : {currentPrice}")
+symbolName='Reliance'
+currentPrice = getPriceBySymbolName(api, tradingSymbolName=symbolName)
+print(f"Current Price of {symbolName} : {currentPrice}")
 
 # Place Order
 #order_response = place_order(api,buy_or_sell='B',tradingsymbol='RELIANCE-EQ',quantity=100)
