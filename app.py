cd8a4848a060c7681d94a2bccc5cfd749cda6d5e
Anshuman <okanshuman@gmail.com>
2025-01-25 13:00:36 +0530

    adding the scheduler to run the selling every 15 seconds

--- app.py
+++ app.py
@@ -24,6 +24,10 @@ holdings_symbols = set()
 # Load purchased stocks from persistent storage (you might want to use a database in production)
 purchased_stocks = set()
 
+# Schedule sell_holding to run every 15 seconds
+scheduler.add_job(func=lambda: sell_holding(api), trigger='interval', seconds=15, id='sell_holding_job')
+
+
 @app.route('/')
 def index():
     fetch_stocks(stock_data, holdings_symbols)
